c0bcfdcc05d73b4d8acf1a6295941d7431ac429a
Tomasz Staskiewicz <tomasz.staskiewicz1989@gmail.com>
2023-12-14 19:46:56 +0100

    inicjalizacja projektu

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int wynik = a + b;
+        System.out.println("wynik: " + wynik);
+
+        }
+    }
